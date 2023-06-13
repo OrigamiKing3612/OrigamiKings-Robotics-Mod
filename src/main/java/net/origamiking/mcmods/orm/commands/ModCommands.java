e27ed63dd485ac5505be6c738829d8eb545f70ca
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2023-06-12 20:23:41 -0500

    Addons (#3)
    Added addon downloader

--- src/main/java/net/origamiking/mcmods/orm/commands/ModCommands.java
+++ src/main/java/net/origamiking/mcmods/orm/commands/ModCommands.java
@@ -3,9 +3,11 @@ package net.origamiking.mcmods.orm.commands;
 
 import net.origamiking.mcmods.oapi.commands.CommandsUtil;
 import net.origamiking.mcmods.orm.OrmMain;
+import net.origamiking.mcmods.orm.commands.download_addons.DownloadAddonsCommands;
 
 public class ModCommands {
     public static void register() {
         CommandsUtil.makeVersionCommand(OrmMain.MOD_ID, OrmMain.VERSION);
+        DownloadAddonsCommands.register();
     }
 }
---
+++ src/main/java/net/origamiking/mcmods/orm/commands/download_addons/DownloadAddonsCommands.java
@@ -0,0 +1,20 @@
+package net.origamiking.mcmods.orm.commands.download_addons;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
+
+public class DownloadAddonsCommands {
+    public static void register() {
+//        downloadAddonCommand("Alacrity", "https://cdn.modrinth.com/data/PUUpX2qq/versions/ZuLy4v1B/Alacrity.zip");
+    }
+    public static void downloadAddonCommand(String addon_name, String url) {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
+                dispatcher.register(CommandManager.literal("orm-download-addon-" + addon_name).executes((context) -> {
+                    (context.getSource()).sendMessage(Text.of("Downloading " + addon_name + " addon..."));
+                    DownloadAddon.downloadAddon(context.getSource(), url, addon_name);
+                    (context.getSource()).sendMessage(Text.of("Download Successful! Go to your Resource Packs to use!"));
+                    return 1;
+                })));
+    }
+}
