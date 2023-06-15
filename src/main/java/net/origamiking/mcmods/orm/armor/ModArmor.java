1fdab24869f7e935d12250caf6e983a5a279ee36
OrigamiKing3612 <origamiking3612@gmail.com>
2023-06-14 21:31:55 -0500

    Removed Robot_0 or car_model from the game

--- src/main/java/net/origamiking/mcmods/orm/armor/ModArmor.java
+++ src/main/java/net/origamiking/mcmods/orm/armor/ModArmor.java
@@ -2,7 +2,6 @@ package net.origamiking.mcmods.orm.armor;
 
 import net.origamiking.mcmods.orm.armor.astrotrain.Astrotrain;
 import net.origamiking.mcmods.orm.armor.bumblebee.Bumblebee;
-import net.origamiking.mcmods.orm.armor.car.Robot_0;
 import net.origamiking.mcmods.orm.armor.galvatron.Galvatron;
 import net.origamiking.mcmods.orm.armor.ironhide.Ironhide;
 import net.origamiking.mcmods.orm.armor.megatron.Megatron;
@@ -16,7 +15,6 @@ import net.origamiking.mcmods.orm.armor.thunder_cracker.ThunderCracker;
 
 public class ModArmor {
     public static void register() {
-        Robot_0.get();
         Skystrike.get();
         Scorpinok.get();
         Starscream.get();
