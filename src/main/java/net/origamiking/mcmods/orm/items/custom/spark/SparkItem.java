8a5585f6cf047985560fb0bc4286646e888ab299
OrigamiKing3612 <origamiking3612@gmail.com>
2023-06-19 15:44:55 -0500

    Added Cybertron Dimension

---
+++ src/main/java/net/origamiking/mcmods/orm/items/custom/spark/SparkItem.java
@@ -0,0 +1,9 @@
+package net.origamiking.mcmods.orm.items.custom.spark;
+
+import net.minecraft.item.Item;
+
+public class SparkItem extends Item {
+    public SparkItem(Settings settings) {
+        super(settings);
+    }
+}
