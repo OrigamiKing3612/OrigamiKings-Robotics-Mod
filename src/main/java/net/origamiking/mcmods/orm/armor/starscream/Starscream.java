8021e8476980461d25d9a73550adff7fafa7427c
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-11 17:11:00 -0500

    Fixed bug

--- src/main/java/net/origamiking/mcmods/orm/armor/starscream/Starscream.java
+++ src/main/java/net/origamiking/mcmods/orm/armor/starscream/Starscream.java
@@ -11,6 +11,6 @@ public class Starscream extends ArmorUtils {
     public static final StarscreamArmorItem CHESTPLATE = registerArmor(OrmMain.MOD_ID, "starscream_chestplate", new StarscreamArmorItem(TransformerArmorMaterial.TRANSFORMER, ArmorItem.Type.CHESTPLATE, new Item.Settings()));
     public static final StarscreamArmorItem LEGGINGS = registerArmor(OrmMain.MOD_ID, "starscream_leggings", new StarscreamArmorItem(TransformerArmorMaterial.TRANSFORMER, ArmorItem.Type.LEGGINGS, new Item.Settings()));
     public static final StarscreamArmorItem BOOTS = registerArmor(OrmMain.MOD_ID, "starscream_boots", new StarscreamArmorItem(TransformerArmorMaterial.TRANSFORMER, ArmorItem.Type.BOOTS, new Item.Settings()));
-    public static final StarscreamJetArmorItem JET = registerArmor(OrmMain.MOD_ID, "starscream_jet", new StarscreamJetArmorItem(TransformerArmorMaterial.TRANSFORMER, ArmorItem.Type.BOOTS, new Item.Settings()));
+    public static final StarscreamJetArmorItem JET = registerArmor(OrmMain.MOD_ID, "starscream_jet", new StarscreamJetArmorItem(TransformerArmorMaterial.TRANSFORMER, ArmorItem.Type.CHESTPLATE, new Item.Settings()));
     public static void get() {}
 }
