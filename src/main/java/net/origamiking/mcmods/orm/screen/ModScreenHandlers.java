5a2934f45f480a88070b7df5edbf826009f0db50
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-18 21:47:11 -0500

    Fixed bug

--- src/main/java/net/origamiking/mcmods/orm/screen/ModScreenHandlers.java
+++ src/main/java/net/origamiking/mcmods/orm/screen/ModScreenHandlers.java
@@ -10,7 +10,7 @@ import net.origamiking.mcmods.orm.screen.refinery.RefineryBlockScreenHandler;
 
 public class ModScreenHandlers {
     public static ScreenHandlerType<RefineryBlockScreenHandler> REFINERY_BLOCK_SCREEN_HANDLER = ScreenUtils.registerScreenHandler(OrmMain.MOD_ID, "refinery", new ScreenHandlerType<>(RefineryBlockScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
-    public static ScreenHandlerType<CompacterBlockScreenHandler> COMPACTER_BLOCK_SCREEN_HANDLER = ScreenUtils.registerScreenHandler(OrmMain.MOD_ID, "compacter.json", new ScreenHandlerType<>(CompacterBlockScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
+    public static ScreenHandlerType<CompacterBlockScreenHandler> COMPACTER_BLOCK_SCREEN_HANDLER = ScreenUtils.registerScreenHandler(OrmMain.MOD_ID, "compacter", new ScreenHandlerType<>(CompacterBlockScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
     public static ScreenHandlerType<ChipRefineryScreenHandler> CHIP_REFINERY_SCREEN_HANDLER = ScreenUtils.registerScreenHandler(OrmMain.MOD_ID, "chip_refinery", new ScreenHandlerType<>(ChipRefineryScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
 
     public static void register() {
