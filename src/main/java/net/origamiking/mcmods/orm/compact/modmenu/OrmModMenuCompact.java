b075efc105da3fddeb69df785041aa07f96200a9
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-05 18:51:13 -0500

    Fixed some bugs

--- src/main/java/net/origamiking/mcmods/orm/compact/modmenu/OrmModMenuCompact.java
+++ src/main/java/net/origamiking/mcmods/orm/compact/modmenu/OrmModMenuCompact.java
@@ -2,11 +2,16 @@ package net.origamiking.mcmods.orm.compact.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 import net.origamiking.mcmods.orm.config.OrmConfigScreen;
 
 public class OrmModMenuCompact implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return OrmConfigScreen::getScreen;
+        if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
+            return OrmConfigScreen::getScreen;
+        } else {
+            return null;
+        }
     }
 }
