a7086c24e6d6dd5f1a69bccf8980dd7bf0a9634a
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-17 17:23:48 -0500

    Finished the EnergyCellItem

--- src/main/java/net/origamiking/mcmods/orm/items/custom/energy_cell/EnergyCellItem.java
+++ src/main/java/net/origamiking/mcmods/orm/items/custom/energy_cell/EnergyCellItem.java
@@ -5,12 +5,12 @@ import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import net.origamiking.mcmods.oapi.armor.ArmorUtils;
 import net.origamiking.mcmods.orm.networking.ModMessages;
+import net.origamiking.mcmods.orm.utils.IEntityEnergyCellsDataSaver;
 import net.origamiking.mcmods.orm.utils.TransformerArmorItem;
 
 public class EnergyCellItem extends Item {
@@ -22,11 +22,16 @@ public class EnergyCellItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         //on both Client and Server
-        if (world.isClient()) ClientPlayNetworking.send(ModMessages.ENERGY_CELLS_ID, PacketByteBufs.create());
-        return TypedActionResult.pass(player.getStackInHand(hand));
-    }
-
-    private static boolean hasTransformerArmorOn(ServerPlayerEntity player) {
-        return ArmorUtils.isArmorSetOfType(player, TransformerArmorItem.class);
+        if (ArmorUtils.isArmorSetOfType(player, TransformerArmorItem.class) && !(((IEntityEnergyCellsDataSaver) player).getPersistentData().getInt("energy_cells") >= 10)) {
+            if (world.isClient()) {
+                ClientPlayNetworking.send(ModMessages.ENERGY_CELLS_ID, PacketByteBufs.create());
+            }
+            if (!player.getAbilities().creativeMode) {
+                player.getStackInHand(hand).decrement(1);
+            }
+            return TypedActionResult.consume(player.getStackInHand(hand));
+        } else {
+            return TypedActionResult.pass(player.getStackInHand(hand));
+        }
     }
 }
