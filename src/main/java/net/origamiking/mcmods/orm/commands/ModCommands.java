d47a2339a80640d6d2faae1242e76568588f6adc
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2023-08-20 15:14:10 -0500

    Change the commands system (#15)
    * Changed the commands
    
    * Changed the Download Addon Command
    
    * Updated changelog

--- src/main/java/net/origamiking/mcmods/orm/commands/ModCommands.java
+++ src/main/java/net/origamiking/mcmods/orm/commands/ModCommands.java
@@ -1,16 +1,38 @@
 package net.origamiking.mcmods.orm.commands;
 
 
-import net.origamiking.mcmods.oapi.commands.CommandsUtil;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Text;
 import net.origamiking.mcmods.orm.OrmMain;
 import net.origamiking.mcmods.orm.commands.download_addons.DownloadAddonsCommands;
 import net.origamiking.mcmods.orm.commands.energy_cell_commands.EnergyCellCommands;
 
 public class ModCommands {
     public static void register() {
-        CommandsUtil.makeVersionCommand(OrmMain.MOD_ID, OrmMain.VERSION);
-        CommandsUtil.linkReturnCommand(OrmMain.MOD_ID + "-wiki", "https://wiki.origamiking.net/orm/ORM.html", "To the ORM wiki!!");
-        DownloadAddonsCommands.register();
-        EnergyCellCommands.register();
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+                    LiteralArgumentBuilder<ServerCommandSource> command = LiteralArgumentBuilder.literal("orm");
+                    EnergyCellCommands.register(command);
+                    command.then(CommandManager.literal("wiki")
+                            .executes(context -> {
+                                context.getSource().sendMessage(Text.literal("Orm Wiki (Click me!)")
+                                        .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://wiki.origamiking.net/orm/ORM.html")))
+                                        .styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.of("To the ORM wiki!!")))));
+                                return 1;
+                            }));
+                    command.then(CommandManager.literal("version")
+                            .executes(context -> {
+                                context.getSource().sendMessage(Text.of("You are on " + OrmMain.VERSION + " of ORM"));
+                                return 1;
+                            }));
+                    DownloadAddonsCommands.register(command);
+
+                    dispatcher.register(command);
+                }
+        );
     }
 }
--- src/main/java/net/origamiking/mcmods/orm/commands/energy_cell_commands/EnergyCellCommands.java
+++ src/main/java/net/origamiking/mcmods/orm/commands/energy_cell_commands/EnergyCellCommands.java
@@ -1,11 +1,9 @@
 package net.origamiking.mcmods.orm.commands.energy_cell_commands;
 
-import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -18,32 +16,17 @@ import net.origamiking.mcmods.orm.utils.IEntityEnergyCellsDataSaver;
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class EnergyCellCommands {
-    public static void register() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
-                make(dispatcher)
+    public static void register(LiteralArgumentBuilder<ServerCommandSource> command) {
+        command.then(CommandManager.literal("energy-cells").requires(source -> source.hasPermissionLevel(2))
+                .then(CommandManager.literal("add").requires(source -> source.hasPermissionLevel(2))
+                        .then(CommandManager.argument("player", EntityArgumentType.player())
+                                .then(CommandManager.argument("amount", IntegerArgumentType.integer())
+                                        .executes(EnergyCellCommands::executeAddEnergyCells))))
+                .then(CommandManager.literal("remove").requires(source -> source.hasPermissionLevel(2))
+                        .then(CommandManager.argument("player", EntityArgumentType.player())
+                                .then(CommandManager.argument("amount", IntegerArgumentType.integer())
+                                        .executes(EnergyCellCommands::executeRemoveEnergyCells))))
         );
-    }
-
-    private static void make(CommandDispatcher<ServerCommandSource> dispatcher) {
-        LiteralArgumentBuilder<ServerCommandSource> command = LiteralArgumentBuilder.literal("orm-energy-cells");
-
-        command.then(CommandManager.literal("add")
-                .then(CommandManager.argument("player", EntityArgumentType.player())
-                        .then(CommandManager.argument("amount", IntegerArgumentType.integer())
-                                .executes(EnergyCellCommands::executeAddEnergyCells)
-                        )
-                )
-        );
-
-        command.then(CommandManager.literal("remove")
-                .then(CommandManager.argument("player", EntityArgumentType.player())
-                        .then(CommandManager.argument("amount", IntegerArgumentType.integer())
-                                .executes(EnergyCellCommands::executeRemoveEnergyCells)
-                        )
-                )
-        );
-
-        dispatcher.register(command);
     }
 
     private static int executeAddEnergyCells(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
